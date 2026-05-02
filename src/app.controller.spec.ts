f69353e9b0dde596851e6c9839056d728de51513
JoaoNaif <joaonaif@gmail.com>
2026-05-02 09:38:45 -0300

    feat: init push

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Vai Corinthians!"', () => {
+      expect(appController.getHello()).toBe('Vai Corinthians!');
     });
   });
 });
